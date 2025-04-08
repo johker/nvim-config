956732e67393f2c93ce57ac076d4f711602bd2f0
johker <johannes.kern@zoho.com>
2025-04-08 07:44:08 +0200

    Added harpoon

--- init.lua
+++ init.lua
@@ -932,6 +932,18 @@ require("lazy").setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
+
+  { -- You can easily change to a different colorscheme.
+    -- Change the name of the colorscheme plugin below, and then
+    -- change the command in the config to whatever the name of that colorscheme is.
+    --
+    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     "folke/tokyonight.nvim",
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
