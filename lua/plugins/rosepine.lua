4eef821ca8184a4a821d8d44a626365194189722
johker <johannes.kern@zoho.com>
2025-04-11 20:20:57 +0200

    Configured rose pine

--- lua/plugins/rosepine.lua
+++ lua/plugins/rosepine.lua
@@ -1,7 +1,33 @@
-return {
+return { -- You can easily change to a different colorscheme.
+  -- Change the name of the colorscheme plugin below, and then
+  -- change the command in the config to whatever the name of that colorscheme is.
+  --
+  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
   "rose-pine/neovim",
   name = "rose-pine",
+  priority = 1000, -- Make sure to load this before all the other start plugins.
   config = function()
-    vim.cmd "colorscheme rose-pine"
+    ---@diagnostic disable-next-line: missing-fields
+    require("rose-pine").setup {
+      styles = {
+        undercurl = true,
+        underline = true,
+        bold = true,
+        italic = false,
+        strikethrough = true,
+        invert_selection = false,
+        invert_signs = false,
+        invert_tabline = false,
+        invert_intend_guides = false,
+        inverse = true, -- invert background for search, diffs, statuslines and errors
+        contrast = "", -- can be "hard", "soft" or empty string
+        palette_overrides = {},
+        overrides = {},
+        dim_inactive = false,
+        transparent_mode = true,
+      },
+    }
+    -- Load the colorscheme here.
+    vim.cmd.colorscheme "rose-pine"
   end,
 }
